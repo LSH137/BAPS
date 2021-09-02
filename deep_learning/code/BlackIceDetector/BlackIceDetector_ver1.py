0ed3ac4bd8ca4d7181767a6e3b459c427a341c9b
Sanghun Lee <dltkdgns4026@gmail.com>
2021-09-03 02:00:20 +0900

    Update BlackIceDetector_ver1.py

--- deep_learning/code/BlackIceDetector/BlackIceDetector_ver1.py
+++ deep_learning/code/BlackIceDetector/BlackIceDetector_ver1.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
-from torch import optim
 from torch.utils.data import TensorDataset, DataLoader
 from sklearn.model_selection import train_test_split
+from torch.autograd import Variable
 import numpy as np
 import DataSet
 
@@ -11,31 +11,67 @@ LENGTH_OF_DATE = 7 # 1week
 EPOCH = 100
 LEARNING_RATE = 0.01
 THRESHOLD = 0.5
-
-# def train(epoch, model, loader_train, optimizer):
-#     model.train()
-
-#     for data, targets in loader_train:
-#         optimizer.zero_grad()
-#         outputs = model(data)
-#         loss = nn.CrossEntropyLoss(outputs, targets)
-
-#         loss.backward()
-#         optimizer.step()
-    
-#     print(f"epoch: {epoch}")
+MINIBATCH = 8
 
 
-def test(model, loader_test):
-    model.eval()
+# learning dataset
+# [relative date, temp, wind velocity, road_type, snowfall, rainfall, relative date ...] ...
+
+if __name__ == "__main__":
+    input_layer = SPECIES_OF_DATA * LENGTH_OF_DATE
+    hidden_layer1_front = int(input_layer / 2.0)
+    hidden_layer1_rear = hidden_layer2_front = hidden_layer2_rear = int(hidden_layer1_front / 2.0)
+    output_layer = 1
+
+    model = nn.Sequential()
+    model.add_module('fc1', nn.Linear(input_layer, hidden_layer1_front))
+    model.add_module('relu1', nn.ReLU())
+    model.add_module('fc2', nn.Linear(hidden_layer1_front, hidden_layer1_rear))
+    model.add_module('relu2', nn.ReLU())
+    model.add_module('fc3', nn.Linear(hidden_layer2_front, hidden_layer2_rear))
+    model.add_module("softmax1", nn.Softmax())
+    model.add_module("fc4", nn.Linear(hidden_layer2_rear, output_layer))
+
+    loss_function = torch.nn.BCELoss()
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+
+    total_x = DataSet.total_x
+    total_y = DataSet.total_y
+
+    learning_dataset, test_dataset_x, crash_dataset, test_dataset_y = train_test_split(total_x, total_y, test_size=1/10, random_state=0)
+
+    learning_dataset = torch.FloatTensor(learning_dataset)
+    test_dataset_x = torch.FloatTensor(test_dataset_x)
+    crash_dataset = torch.FloatTensor(crash_dataset)
+    test_dataset_y = torch.FloatTensor(test_dataset_y)
+
+    dataset_train = TensorDataset(learning_dataset, crash_dataset)
+    dataset_test = TensorDataset(test_dataset_x, test_dataset_y)
+
+    loader_train = DataLoader(dataset_train, batch_size=MINIBATCH, shuffle=True)
+    loader_test = DataLoader(dataset_test, batch_size=MINIBATCH, shuffle=False)
+
+    model.train() # set train mode
+    for epoch in range(EPOCH):
+        for data, targets in loader_train:          
+            outputs = model(data)      
+            loss = loss_function(outputs, targets)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f"epoch: {epoch} | loss = {round(loss.item()*100.0, 2)} %")
+
+    print("==== after learning ====")
+    print("----- TEST -----")
+    model.eval() # set test mode
     correct = 0
 
     with torch.no_grad():
         for data, target in loader_test:
             outputs = model(data)
-            #print(f"test-output = {outputs.data}")
-            #print(f"target = {target.data}")
-            #print(f"data = {data.data}")
             for index in range(len(data)):
                 if outputs[index] > THRESHOLD: # predict there is crash
                     if target[index][0] == 1: # crash
@@ -45,69 +81,4 @@ def test(model, loader_test):
                         correct += 1
     
     data_num = len(loader_test.dataset)
-    print(f"accuracy: {float(correct) / float(data_num) * 100}")
-
-    
-# learning dataset
-# [relative date, temp, wind velocity, road_type, snowfall, rainfall, relative date ...] ...
-
-if __name__ == "__main__":
-    input_layer = SPECIES_OF_DATA * LENGTH_OF_DATE
-    hidden_layer1 = int(input_layer / 2.0)
-    hidden_layer2 = int(hidden_layer1 / 2.0)
-    output_layer = 1
-
-    model = nn.Sequential()
-    model.add_module('fc1', nn.Linear(input_layer, hidden_layer1))
-    model.add_module('relu1', nn.ReLU())
-    model.add_module('fc2', nn.Linear(hidden_layer1, hidden_layer2))
-    model.add_module('relu2', nn.ReLU())
-    model.add_module('fc3', nn.Linear(hidden_layer2, output_layer))
-
-    loss_function = nn.CrossEntropyLoss()
-
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-
-    total_x = DataSet.total_x
-    total_y = DataSet.total_y
-
-    learning_dataset, test_dataset_x, crash_dataset, test_dataset_y = train_test_split(total_x, total_y, test_size=1/10, random_state=0)
-
-    learning_dataset = torch.FloatTensor(learning_dataset)
-    test_dataset_x = torch.FloatTensor(test_dataset_x)
-    crash_dataset = torch.LongTensor(crash_dataset)
-    test_dataset_y = torch.LongTensor(test_dataset_y)
-
-    dataset_train = TensorDataset(learning_dataset, crash_dataset)
-    dataset_test = TensorDataset(test_dataset_x, test_dataset_y)
-
-    loader_train = DataLoader(dataset_train, batch_size=8, shuffle=True)
-    loader_test = DataLoader(dataset_test, batch_size=8, shuffle=False)
-
-    for data, target in loader_train:
-        print(f"loader_train = {data}, {target}")
-
-    for data, target in loader_test:
-        print(f"loader_test = {data}, {target}")
-
-    test(model, loader_test)
-
-    model.train()
-    for epoch in range(EPOCH):
-        for data, targets in loader_train:
-            print(f"data = {data}, len = {len(data)}")
-            print(f"target = {targets}, len = {len(targets)}")
-            
-            outputs = model(data)
-            print(f"output = {outputs.data}, len = {len(outputs)}")
-            print(f"shape: output = {outputs.shape}, targets = {targets.shape}")
-            loss = loss_function(outputs, targets)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        print(f"epoch: {epoch} | loss = {loss.item()}")
-
-    print("==== after learning ====")
-    test(model, loader_test)
+    print(f"accuracy: {float(correct) / float(data_num) * 100} %")
